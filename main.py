3b0d6923a52dbb22b5bbd5f99335751d29a17f34
neas <neas@carbungle.com>
2026-08-16 17:30:54 -0700

    v10: add pyautoit to requirements, bump version

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ import numpy as np
 
 import ctypes as _ctypes
 
-VERSION = "9"
+VERSION = "10"
 UPDATE_BASE = "https://raw.githubusercontent.com/carbungle/fishbot/main"
 UPDATE_FILES = ["main.py", "auth.py"]
 
